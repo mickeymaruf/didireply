dd1ffebc618a2e903dfd4178ea08700397c6a412
mickeymaruf <mickeymaruf@gmail.com>
2025-02-13 19:20:19 +0600

    fix: Fixed sidepanel open issue

--- background.js
+++ background.js
@@ -1,25 +1,11 @@
-const LINKEDIN_ORIGIN = "https://www.linkedin.com";
-
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
-
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-  if (!tab.url) return;
-  const url = new URL(tab.url);
-  // Enables the side panel on google.com
-  if (url.origin === LINKEDIN_ORIGIN) {
-    await chrome.sidePanel.setOptions({
-      tabId,
+chrome.action.onClicked.addListener(function (tab) {
+  // Check if the tab is already in the right state
+  if (tab.url && tab.url.includes("linkedin.com")) {
+    // Open the side panel on click
+    chrome.sidePanel.setOptions({
+      // Display the side panel with your content
       path: "sidepanel.html",
       enabled: true,
-    });
-  } else {
-    // Disables the side panel on all other sites
-    await chrome.sidePanel.setOptions({
-      tabId,
-      enabled: false,
     });
   }
 });
